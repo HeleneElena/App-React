7d4ab145b668ecfa9bc6be7e12c9a0b041d6881c
HeleneElena <heleneebert1@gmail.com>
2021-11-17 16:58:46 +0100

    commit2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://w7.pngwing.com/pngs/324/209/png-transparent-letters-abc-education-gold-golden-gloss-alphabet-literacy-illiterate-illiteracy.png'/>
+      </header>  
+      <nav className='nav'> 
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+
+      <div className='content'> 
+        <div> 
+            <img src='https://eefreedom.files.wordpress.com/2018/12/cropped-eebeach1280x800.jpg'/>
+        </div>
+        <div> 
+            avatar + description
+        </div>
+        <div> 
+            My posts
+              <div> 
+                New post
+              </div>
+              <div>
+                <div> Post 1</div>
+                <div> Post 2</div>
+                <div> Post 3</div>
+              </div>
+        </div>
+        <div> </div>
+        <div> </div>
+      </div>
     </div>
+
   );
 }
+
+
 
 export default App;
